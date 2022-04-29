89f5fa3800a71b77d1bdf4f46e99200f5c7bd655
Manabu Niseki <manabu.niseki@gmail.com>
2022-04-29 11:21:27 +0900

    refactor: enrich Service

---
+++ tests/test_bom.py
@@ -0,0 +1,9 @@
+from pycomponents import __version__
+from pycomponents.bom import get_tool
+
+
+def test_get_tool():
+    tool = get_tool()
+    assert tool.vendor == "ninoseki"
+    assert tool.name == "pycomponents - https://github.com/ninoseki/pycomponents"
+    assert tool.version == __version__
